0c4a59657f80cbf1c48a7eb0d314555e9b4312d7
Dragos Ct <dragosct10@gmail.com>
2016-08-31 16:42:12 +0300

    moving-tab

--- assets/js/wizard.js
+++ assets/js/wizard.js
@@ -1,193 +1,199 @@
 searchVisible = 0;
-transparent = true;
+        transparent = true;
 
-$(document).ready(function(){
-    /*  Activate the tooltips      */
-    $('[rel="tooltip"]').tooltip();
+        $(document).ready(function(){
 
-    $('.wizard-card').bootstrapWizard({
-        'tabClass': 'nav nav-pills',
-        'nextSelector': '.btn-next',
-        'previousSelector': '.btn-previous',
+            /*  Activate the tooltips      */
+            $('[rel="tooltip"]').tooltip();
 
-         onInit : function(tab, navigation, index){
+            // Code for the Validator
+            var $validator = $('.wizard-card form').validate({
+        		  rules: {
+        		    firstname: {
+        		      required: true,
+        		      minlength: 3
+        		    },
+        		    lastname: {
+        		      required: true,
+        		      minlength: 3
+        		    },
+        		    email: {
+        		      required: true,
+        		      minlength: 3,
+        		    }
+                },
 
-           //check number of tabs and fill the entire row
-           var $total = navigation.find('li').length;
-           $width = 100/$total;
+                errorPlacement: function(error, element) {
+                    $(element).parent('div').addClass('has-error');
+                 }
+        	});
 
-           $display_width = $(document).width();
+            // Wizard Initialization
+          	$('.wizard-card').bootstrapWizard({
+                'tabClass': 'nav nav-pills',
+                'nextSelector': '.btn-next',
+                'previousSelector': '.btn-previous',
 
-           if($display_width < 600 && $total > 3){
-               $width = 50;
-           }
+                onNext: function(tab, navigation, index) {
+                	var $valid = $('.wizard-card form').valid();
+                	if(!$valid) {
+                		$validator.focusInvalid();
+                		return false;
+                	}
+                },
 
-           navigation.find('li').css('width',$width + '%');
+                onInit : function(tab, navigation, index){
 
-        },
-        onNext: function(tab, navigation, index){
-            if(index == 1){
-                return validateFirstStep();
-            } else if(index == 2){
-                return validateSecondStep();
-            } else if(index == 3){
-                return validateThirdStep();
-            } //etc.
+                  //check number of tabs and fill the entire row
+                  var $total = navigation.find('li').length;
+                  $width = 100/$total;
+                  var $wizard = navigation.closest('.wizard-card');
 
-        },
-        onTabClick : function(tab, navigation, index){
-            // Disable the posibility to click on tabs
-            return false;
-        },
-        onTabShow: function(tab, navigation, index) {
-            var $total = navigation.find('li').length;
-            var $current = index+1;
+                  $display_width = $(document).width();
 
-            var wizard = navigation.closest('.wizard-card');
+                  if($display_width < 600 && $total > 3){
+                      $width = 50;
+                  }
 
-            // If it's the last tab then hide the last button and show the finish instead
-            if($current >= $total) {
-                $(wizard).find('.btn-next').hide();
-                $(wizard).find('.btn-finish').show();
-            } else {
-                $(wizard).find('.btn-next').show();
-                $(wizard).find('.btn-finish').hide();
+                   navigation.find('li').css('width',$width + '%');
+                   $first_li = navigation.find('li:first-child a').html();
+                   $moving_div = $('<div class="moving-tab">' + $first_li + '</div>');
+                   $('.wizard-card .wizard-navigation').append($moving_div);
+                   refreshAnimation($wizard, index);
+                   $('.moving-tab').css('transition','transform 0s');
+               },
+
+                onTabClick : function(tab, navigation, index){
+                    // Disable the posibility to click on tabs
+                    return false;
+                },
+
+                onTabShow: function(tab, navigation, index) {
+                    var $total = navigation.find('li').length;
+                    var $current = index+1;
+
+                    var $wizard = navigation.closest('.wizard-card');
+
+                    // If it's the last tab then hide the last button and show the finish instead
+                    if($current >= $total) {
+                        $($wizard).find('.btn-next').hide();
+                        $($wizard).find('.btn-finish').show();
+                    } else {
+                        $($wizard).find('.btn-next').show();
+                        $($wizard).find('.btn-finish').hide();
+                    }
+
+                    button_text = navigation.find('li:nth-child(' + $current + ') a').html();
+
+                    setTimeout(function(){
+                        $('.moving-tab').text(button_text);
+                    }, 150);
+
+                    var checkbox = $('.footer-checkbox');
+
+                    if( !index == 0 ){
+                        $(checkbox).css({
+                            'opacity':'0',
+                            'visibility':'hidden',
+                            'position':'absolute'
+                        });
+                    } else {
+                        $(checkbox).css({
+                            'opacity':'1',
+                            'visibility':'visible'
+                        });
+                    }
+
+                    refreshAnimation($wizard, index);
+                }
+          	});
+
+
+            // Prepare the preview for profile picture
+            $("#wizard-picture").change(function(){
+                readURL(this);
+            });
+
+            $('[data-toggle="wizard-radio"]').click(function(){
+                wizard = $(this).closest('.wizard-card');
+                wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
+                $(this).addClass('active');
+                $(wizard).find('[type="radio"]').removeAttr('checked');
+                $(this).find('[type="radio"]').attr('checked','true');
+            });
+
+            $('[data-toggle="wizard-checkbox"]').click(function(){
+                if( $(this).hasClass('active')){
+                    $(this).removeClass('active');
+                    $(this).find('[type="checkbox"]').removeAttr('checked');
+                } else {
+                    $(this).addClass('active');
+                    $(this).find('[type="checkbox"]').attr('checked','true');
+                }
+            });
+
+            $('.set-full-height').css('height', 'auto');
+
+        });
+
+
+
+         //Function to show image before upload
+
+        function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
+                }
+                reader.readAsDataURL(input.files[0]);
             }
         }
-    });
 
-    // Prepare the preview for profile picture
-    $("#wizard-picture").change(function(){
-        readURL(this);
-    });
+        $(window).resize(function(){
+            $('.wizard-card').each(function(){
+                $wizard = $(this);
+                index = $wizard.bootstrapWizard('currentIndex');
+                refreshAnimation($wizard, index);
 
-    $('[data-toggle="wizard-radio"]').click(function(){
-        wizard = $(this).closest('.wizard-card');
-        wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
-        $(this).addClass('active');
-        $(wizard).find('[type="radio"]').removeAttr('checked');
-        $(this).find('[type="radio"]').attr('checked','true');
-    });
+                $('.moving-tab').css({
+                    'transition': 'transform 0s'
+                });
+            });
+        });
 
-    $('[data-toggle="wizard-checkbox"]').click(function(){
-        if( $(this).hasClass('active')){
-            $(this).removeClass('active');
-            $(this).find('[type="checkbox"]').removeAttr('checked');
-        } else {
-            $(this).addClass('active');
-            $(this).find('[type="checkbox"]').attr('checked','true');
+        function refreshAnimation($wizard, index){
+            total_steps = $wizard.find('li').length;
+            move_distance = $wizard.width() / total_steps;
+            step_width = move_distance;
+            move_distance *= index;
+
+            $current = index + 1;
+
+            if($current == 1){
+                move_distance -= 8;
+            } else if($current == total_steps){
+                move_distance += 8;
+            }
+
+            $wizard.find('.moving-tab').css('width', step_width);
+            $('.moving-tab').css({
+                'transform':'translate3d(' + move_distance + 'px, 0, 0)',
+                'transition': 'all 0.5s cubic-bezier(0.29, 1.42, 0.79, 1)'
+
+            });
         }
-    });
 
-
-    $('.set-full-height').css('height', 'auto');
-
-});
-
-function validateFirstStep(){
-
-    $(".wizard-card form").validate({
-		rules: {
-			firstname: "required",
-			lastname: "required",
-			email: {
-				required: true,
-				email: true
-			}
-
-/*  other possible input validations
-			,username: {
-				required: true,
-				minlength: 2
-			},
-			password: {
-				required: true,
-				minlength: 5
-			},
-			confirm_password: {
-				required: true,
-				minlength: 5,
-				equalTo: "#password"
-			},
-
-			topic: {
-				required: "#newsletter:checked",
-				minlength: 2
-			},
-			agree: "required"
-*/
-
-		},
-		messages: {
-			firstname: "Please enter your First Name",
-			lastname: "Please enter your Last Name",
-			email: "Please enter a valid email address",
-
-/*   other posible validation messages
-			username: {
-				required: "Please enter a username",
-				minlength: "Your username must consist of at least 2 characters"
-			},
-			password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long"
-			},
-			confirm_password: {
-				required: "Please provide a password",
-				minlength: "Your password must be at least 5 characters long",
-				equalTo: "Please enter the same password as above"
-			},
-			email: "Please enter a valid email address",
-			agree: "Please accept our policy",
-			topic: "Please select at least 2 topics"
-*/
-
-		}
-	});
-
-	if(!$(".wizard-card form").valid()){
-    	//form is invalid
-    	return false;
-	}
-
-	return true;
-}
-
-function validateSecondStep(){
-
-    //code here for second step
-    $(".wizard-card form").validate({
-		rules: {
-
-		},
-		messages: {
-
-		}
-	});
-
-	if(!$(".wizard-card form").valid()){
-    	console.log('invalid');
-    	return false;
-	}
-	return true;
-
-}
-
-function validateThirdStep(){
-    //code here for third step
-
-
-}
-
- //Function to show image before upload
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#wizardPicturePreview').attr('src', e.target.result).fadeIn('slow');
-        }
-        reader.readAsDataURL(input.files[0]);
-    }
-}
+        function debounce(func, wait, immediate) {
+        	var timeout;
+        	return function() {
+        		var context = this, args = arguments;
+        		clearTimeout(timeout);
+        		timeout = setTimeout(function() {
+        			timeout = null;
+        			if (!immediate) func.apply(context, args);
+        		}, wait);
+        		if (immediate && !timeout) func.apply(context, args);
+        	};
+        };
